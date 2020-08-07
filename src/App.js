6ce28be33ae8fc6f5ad4a1d90f68c7f18150805e
dharanarola2012@gmail.com <dharanarola2012@gmail.com>
2020-08-07 14:46:36 -0400

    Counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import FirstComponent from './components/learning-example/FirstComponent';
+import SecondComponent from './components/learning-example/SecondComponent';
+import ThirdComponent from './components/learning-example/ThirdComponent';
+import Counter from './components/counter/counter';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+
+      <Counter />
+
     </div>
   );
 }
 
+class LearingsComponent extends Component {
+  render() {
+    return (
+      <div className="LearningComponent" >
+        <FirstComponent></FirstComponent>
+        <SecondComponent></SecondComponent>
+        <ThirdComponent></ThirdComponent>
+      </div>
+    );
+  }
+}
 export default App;
